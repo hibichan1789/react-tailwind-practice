b403a202f3d5cecb8de14780f3c377b75c56a441
hibiki <takechi.hibiki1234@gmail.com>
2026-02-27 01:57:00 +0900

    コンポーネントの練習

--- src/App.tsx
+++ src/App.tsx
@@ -254,8 +254,147 @@ function SearchBar(){
   );
 }
 //absolute top-1/2 -translate-y-1/2　これでアイコンとかを検索窓にかぶせることができる
+function StatusBadge(){
+  return(
+    <span className="inline-flex items-center px-2.5 py-0.5 bg-green-100 rounded-full text-xs text-green-800 font-medium whitespace-nowrap leading-none
+        hover:bg-green-300 cursor-pointer">
+      稼働中
+    </span>
+  );
+}
+
+function NotificationTab(){
+  return(
+    <div className="flex gap-8 px-3 py-4">
+      <div className="relative inline-flex items-center">
+        <p className="bg-indigo-200 text-gray-600 font-bold text-md px-2 py-1 rounded-lg hover:bg-indigo-400 hover:text-gray-800 transition-all duration-100 cursor-pointer">
+          メッセージ1
+        </p>
+        <div className="absolute -top-2 -right-2.5 w-5 h-5 bg-red-500 text-white text-xs font-mono flex items-center justify-center leading-none rounded-full border border-gray-300">
+          1
+        </div>
+        
+      </div>
+            <div className="relative inline-flex items-center">
+        <p className="bg-indigo-200 text-gray-600 font-bold text-md px-2 py-1 rounded-lg hover:bg-indigo-400 hover:text-gray-800 transition-all duration-100 cursor-pointer">
+          メッセージ2
+        </p>
+        <div className="absolute -top-2 -right-2.5 w-5 h-5 bg-red-500 text-white text-xs font-mono flex items-center justify-center leading-none rounded-full border border-gray-300">
+          1
+        </div>
+        
+      </div>
+            <div className="relative inline-flex items-center">
+        <p className="bg-indigo-200 text-gray-600 font-bold text-md px-2 py-1 rounded-lg hover:bg-indigo-400 hover:text-gray-800 transition-all duration-100 cursor-pointer">
+          メッセージ3
+        </p>
+        <div className="absolute -top-2 -right-2.5 w-5 h-5 bg-red-500 text-white text-xs font-mono flex items-center justify-center leading-none rounded-full border border-gray-300">
+          1
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+function AvatarStack(){
+  return(
+    <div className="flex -space-x-4">
+      <div className="w-10 h-10 rounded-full border-2 border-gray-200 bg-blue-400 z-10 hover:z-50 transition-all"></div>
+      <div className="w-10 h-10 rounded-full border-2 border-gray-200 bg-green-400 z-20 hover:z-50 transition-all"></div>
+      <div className="w-10 h-10 rounded-full border-2 border-gray-200 bg-yellow-400 z-30 hover:z-50 transition-all"></div>
+    </div>
+  );
+}
+function StackCard(){
+  return(
+    <div className="relative w-64 h-40 m-5">
+      <div className="absolute top-4 right-4 w-full h-full bg-gray-200 rounded-xl z-0"></div>
+      <div className="absolute top-0 right-0 w-full h-full border bg-white border-gray-300 shadow-xl rounded-xl z-10
+      hover:-translate-y-2 hover:shadow-2xl transition-all cursor-pointer flex items-center justify-center">
+        メインコンテンツ
+      </div>
+    </div>
+  );
+}
+
+function PhotoBadge(){
+  return(
+    <div className="w-64">
+      <div className="h-40 bg-blue-200 rounded-lg"></div>
+      <div className="-mt-6 ml-4 w-fit py-2 bg-black text-white rounded-md shadow-md rotate-2 cursor-pointer">
+        New Arrival
+      </div>
+    </div>
+  );
+}
+function MemoCard(){
+  return(
+    <div className="relative w-64 h-64 bg-yellow-50 p-3 shadow-md border border-gray-200 m-10 group cursor-pointer">
+      <p>今日のタスク</p>
+      <p className="absolute -top-3 -right-3 bg-gray-300 px-3 py-1 rounded-md shadow-md rotate-3 group-hover:rotate-0 transition-all">sample memo</p>
+    </div>
+  );
+}
+
+function ArticleImage(){
+  return(
+    <div className="w-80 border rounded-xl overflow-hidden bg-white cursor-pointer shadow-xl">
+      <img 
+        src="https://images.unsplash.com/photo-1498050108023-c5249f4df085" alt="PC"
+        className="w-full aspect-video object-cover"
+      />
+      <div className="p-4">
+        <p className="font-bold">Tailwindの実践テクニック</p>
+      </div>
+    </div>
+  );
+}//object-cover これは縦横比を変えずに拡大縮小してくれる
+
+function ZoomPhoto(){
+  return(
+    <div className="relative min-w-77 w-full rounded-2xl overflow-hidden border-2 border-gray-400 shadow-lg cursor-pointer group">
+      <img 
+        src="https://picsum.photos/400" alt="" 
+        className="w-full aspect-square object-cover group-hover:scale-110 transition-transform duration-500"
+      />
+      <a href="https://google.com" className="absolute bottom-4 left-4 text-white font-bold shadow-md bg-gray-500/40 px-2 py-1 rounded-md hover:bg-gray-500/90 transition-colors duration-100">詳細</a>
+    </div>
+  );
+}
+
+function ResponsiveList(){
+  return(
+    <div>
+      
+      <div className="relative container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+          <div className="bg-blue-300 p-6 rounded-lg shadow  text-center">Card1</div>
+          <div className="bg-blue-400 p-6 rounded-lg shadow  text-center">Card2</div>
+          <div className="bg-blue-500 p-6 rounded-lg shadow  text-center">Card3</div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+function ResponsivePhoto(){
+  const array = new Array(10).fill(null);
+  return(
+    <div className="flex flex-col items-center gap-8">
+      <h2 className="text-3xl font-bold w-fit bg-linear-to-r from-amber-600 to-yellow-500 px-4 pt-2 pb-4 rounded-lg shadow-2xl text-gray-200 leading-none">My Gallery</h2>
+      <div className="container mim-h-screen">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {array.map((_, index)=><ZoomPhoto key={index}/>)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function TailWindApp(){
   return(
-    <SearchBar/>
+    <ResponsivePhoto/>
   );
 }
